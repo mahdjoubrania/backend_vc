55afd958e042f025286649438cbdf0c64397e420
mahdjoubrania <mahdjoubrania19@gmail.com>
2026-08-26 10:12:42 +0100

    Update frontend files and API connection

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -1,23 +1,23 @@
-
 const db = require('../config/db');
 const bcrypt = require('bcryptjs');
-// 1. Login
 
+// التسجيل برقم الهاتف كلمة المرور
 exports.login = async (req, res) => {
-  const { role, password } = req.body;
+  const { phone, password } = req.body;
 
-  if (!role || !password) {
-    return res.status(400).json({ message: 'Veuillez fournir le role et le mot de passe.' });
+  if (!phone || !password) {
+    return res.status(400).json({ message: 'Veuillez fournir le numéro de téléphone et le mot de passe.' });
   }
 
   try {
+    // البحث بواسطة رقم الهاتف
     const [users] = await db.query(
-      'SELECT id, full_name AS fullName, phone, role, password_hash, is_active AS isActive FROM users WHERE role = ?',
-      [role]
+      'SELECT id, full_name AS fullName, phone, role, password_hash, is_active AS isActive FROM users WHERE phone = ?',
+      [phone]
     );
 
     if (users.length === 0) {
-      return res.status(401).json({ message: 'role ou mot de passe incorrect.' });
+      return res.status(401).json({ message: 'Numéro de téléphone ou mot de passe incorrect.' });
     }
 
     const user = users[0];
@@ -28,7 +28,7 @@ exports.login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password_hash);
     if (!isMatch) {
-      return res.status(401).json({ message: 'le role ou mot de passe incorrect.' });
+      return res.status(401).json({ message: 'Numéro de téléphone ou mot de passe incorrect.' });
     }
 
     delete user.password_hash;
