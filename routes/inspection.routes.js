d07c230fb16deb2bee4e920c45d7a961c0797b6a
mahdjoubrania <mahdjoubrania19@gmail.com>
2026-09-13 10:08:27 +0100

    fix inspection controller path

--- routes/inspection.routes.js
+++ routes/inspection.routes.js
@@ -1,26 +1,3 @@
-
-const express = require('express');
-const router = express.Router();
-const inspectionCtrl = require('../verification/inspection.controller'); 
-const verifyToken = require('../middleware/auth.middleware');
-
-// مسارات الحفظ
-router.post('/kilometrage', verifyToken, inspectionCtrl.saveKilometrage); 
-router.post('/scanner', verifyToken, inspectionCtrl.saveScanner); 
-router.post('/moteur', verifyToken, inspectionCtrl.saveMoteur); 
-router.post('/suspension', verifyToken, inspectionCtrl.saveSuspension); 
-router.post('/tole', verifyToken, inspectionCtrl.saveTole); 
-router.post('/general', verifyToken, inspectionCtrl.saveGeneral); 
-
-
-// GET routes
-router.get('/all', verifyToken, inspectionCtrl.getAllInspections); 
-router.get('/tole/:id', verifyToken, inspectionCtrl.getToleReportById);
-router.get('/tole-report/:id', verifyToken, inspectionCtrl.getToleReportById); 
-router.get('/details/:inspection_id', verifyToken, inspectionCtrl.getInspectionDetails);
-module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const inspectionCtrl = require('../verification/inspection.controller');
